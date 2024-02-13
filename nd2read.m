a97a0235ed8561d602cbba382c2bfbc64d2c43d1
Lena J Gschossmann <33325540+LenaGschossmann@users.noreply.github.com>
2024-02-13 22:47:48 +0100

    fixed error about too many inputs for fseek and made third channel optional

--- nd2read.m
+++ nd2read.m
@@ -5,14 +5,20 @@ disp(['analyzing file structure used ', sprintf('%0.2f', toc), ' seconds'])
 
 im_ch1 = zeros(finfo.img_width, finfo.img_height, 'uint16');
 im_ch2 = zeros(finfo.img_width, finfo.img_height, 'uint16');
-im_ch3 = zeros(finfo.img_width, finfo.img_height, 'uint16');
-if finfo.ch_count == 4
+if finfo.ch_count == 3
+    im_ch3 = zeros(finfo.img_width, finfo.img_height, 'uint16');
+elseif finfo.ch_count == 4
   im_ch4 = zeros(finfo.img_width, finfo.img_height, 'uint16');
+else
+    im_ch3 = [];
 end
 
 fid = fopen(filename, 'r');
-fseek(fid, finfo.file_structure(strncmp('ImageDataSeq', ...
-  {finfo.file_structure(:).nameAttribute}, 12)).dataStartPos, 'bof');
+filepos = find(strncmp('ImageDataSeq|0!',{finfo.file_structure(:).nameAttribute}, 14),1);
+fseek(fid,finfo.file_structure(filepos).dataStartPos, 'bof');
+
+% fseek(fid, finfo.file_structure(strncmp('ImageDataSeq', ...
+%   {finfo.file_structure(:).nameAttribute}, 12)).dataStartPos, 'bof');
 
 tic
 % Image extracted from ND2 has image width defined by its first dimension.
@@ -27,7 +33,7 @@ if finfo.padding_style == 1
         im_ch4(:, ii) = temp(4, :);
         fseek(fid, 2, 'cof');
     end
-  else
+  elseif finfo.ch_count == 3
     for ii = 1: finfo.img_height
         temp = reshape(fread(fid, finfo.ch_count * finfo.img_width, '*uint16'),...
           [finfo.ch_count finfo.img_width]);
@@ -36,6 +42,14 @@ if finfo.padding_style == 1
         im_ch2(:, ii) = temp(3, :);
         fseek(fid, 2, 'cof');
     end
+  else
+     for ii = 1: finfo.img_height
+        temp = reshape(fread(fid, finfo.ch_count * finfo.img_width, '*uint16'),...
+          [finfo.ch_count finfo.img_width]);
+        im_ch1(:, ii) = temp(2, :);
+        im_ch2(:, ii) = temp(3, :);
+        fseek(fid, 2, 'cof');
+    end 
   end
 else
   if finfo.ch_count == 4
@@ -47,13 +61,20 @@ else
         im_ch3(:, ii) = temp(3, :);
         im_ch4(:, ii) = temp(4, :);
     end
-  else
+  elseif finfo.ch_count == 3
     for ii = 1: finfo.img_height
         temp = reshape(fread(fid, finfo.ch_count * finfo.img_width, '*uint16'),...
           [finfo.ch_count finfo.img_width]);
         im_ch1(:, ii) = temp(1, :);
         im_ch2(:, ii) = temp(2, :);
         im_ch3(:, ii) = temp(3, :);
+    end 
+  else
+      for ii = 1: finfo.img_height
+        temp = reshape(fread(fid, finfo.ch_count * finfo.img_width, '*uint16'),...
+          [finfo.ch_count finfo.img_width]);
+        im_ch1(:, ii) = temp(1, :);
+        im_ch2(:, ii) = temp(2, :);
     end 
   end
 end
@@ -62,8 +83,10 @@ fclose(fid);
 
 im_ch1 = permute(im_ch1, [2 1]);
 im_ch2 = permute(im_ch2, [2 1]);
-im_ch3 = permute(im_ch3, [2 1]);
-if finfo.ch_count == 4
+
+if finfo.ch_count == 3
+    im_ch3 = permute(im_ch3, [2 1]);
+elseif finfo.ch_count == 4
     im_ch4 = permute(im_ch4, [2 1]);
 end
 if any(strcmpi(varargin, 'use_ch4'))
